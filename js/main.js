bcd89850c8f525931e6e4095feaeaaf535ef2e52
Matheus <mat.hen.gra@gmail.com>
2023-01-18 21:58:34 -0300

    atual.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 function submitRate() {
-    const rate = document.querySelector('input[name="rate"]:checked').value;
-    const rateSelected = document.querySelector('.rate-selected');
-    const flipCard = document.querySelector('.flip-card-inner');
+    //const rate = document.querySelector('input[name="rate"]:checked').value
+    const rateSelected = document.querySelector('.rate-selected')
+    const flipCard = document.querySelector('.flip-card-inner')
 
-    rateSelected.innerText = `You selected ${rate} out of 5`;
-    flipCard.style.transform = "rotateY(-180deg)";
+    //rateSelected.innerText = `You selected ${rate} out of 5`
+    flipCard.style.transform = "rotateY(-180deg)"
 }
 
 /*function submitRate() {
-    const formulary = document.getElementsByClassName("form")[0];
-    const rate = formulary.elements["rate"].value;
-    const rateSelected = document.querySelector('.rate-selected');
-    const flipCard = document.querySelector('.flip-card-inner');
+    const formulary = document.getElementsByClassName("form")[0]
+    const rate = formulary.elements["rate"].value
+    const rateSelected = document.querySelector('.rate-selected')
+    const flipCard = document.querySelector('.flip-card-inner')
 
-    rateSelected.innerText = `You selected ${rate} out of 5`;
-    flipCard.style.transform = "rotateY(-180deg)";
+    rateSelected.innerText = `You selected ${rate} out of 5`
+    flipCard.style.transform = "rotateY(-180deg)"
 }*/
