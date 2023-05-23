4060f7f9438be35649005848ed8ac079330a8855
Matheus <mat.hen.gra@gmail.com>
2023-05-23 11:25:10 -0300

    changed submit method
    add preventDefault; add eventListener on form submit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-function submitRate() {
-    const rate = document.querySelector('input[name="rate"]:checked').value
-    const rateSelected = document.querySelector('.rate-selected')
-    const flipCard = document.querySelector('.flip-card-inner')
+const form = document.querySelector('.form');
+form.addEventListener('submit', (submitRate))
 
-    rateSelected.innerText = `You selected ${rate} out of 5`
-    flipCard.style.transform = "rotateY(-180deg)"
+function submitRate(e) {
+    e.preventDefault();
+    const rate = document.querySelector('input[name="rate"]:checked').value;
+    const rateSelected = document.querySelector('.rate-selected');
+    const flipCard = document.querySelector('.flip-card-inner');
+
+    rateSelected.innerText = `You selected ${rate} out of 5`;
+    flipCard.style.transform = "rotateY(-180deg)";
 }
 
-/*function submitRate() {
-    const formulary = document.getElementsByClassName("form")[0]
-    const rate = formulary.elements["rate"].value
+/* function submitRate(e) {
+    e.preventDefault();
+    const rate = form.elements["rate"].value
     const rateSelected = document.querySelector('.rate-selected')
     const flipCard = document.querySelector('.flip-card-inner')
 
     rateSelected.innerText = `You selected ${rate} out of 5`
     flipCard.style.transform = "rotateY(-180deg)"
-}*/
+} */
